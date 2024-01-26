5d43ef48db79c2b4cfebbfee10e8a055a0826d7d
Daniel Ochoa <ing.danielochoa@hotmail.com>
2024-01-25 21:37:14 -0500

    change Files

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce',
-};
